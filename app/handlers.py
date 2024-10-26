a85422ccf85f01a095786f902bcd5b0a944bbb5b
denzizigzj <70101673+denzizigzj@users.noreply.github.com>
2024-10-26 20:16:20 +0300

    Add files via upload

---
+++ app/handlers.py
@@ -0,0 +1,46 @@
+from aiogram import F, Router
+from aiogram.types import Message, CallbackQuery, FSInputFile
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+import app.keyboards as kb
+import app.database.requests as rq
+
+router = Router()
+
+async def send_welcome(message: Message):
+    photo = FSInputFile("photo/mospy.jpg") 
+    caption = "Привет, это бот ДПО Московского Политеха"
+    await message.answer_photo(photo=photo, caption=caption)
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await rq.set_user(message.from_user.id)
+    await send_welcome(message)
+
+
+@router.message(F.text == 'Каталог')
+async def catalog(message: Message):
+    await message.answer('Выберите категорию товара', reply_markup=await kb.categories())
+
+
+@router.callback_query(F.data.startswith('category_'))
+async def category(callback: CallbackQuery):
+    await callback.answer('Вы выбрали категорию')
+    await callback.message.answer('Выберите товар по категории',
+                                  reply_markup=await kb.items(callback.data.split('_')[1]))
+
+
+@router.callback_query(F.data.startswith('item_'))
+async def handle_name(callback: CallbackQuery):
+    item_data = await rq.get_item(callback.data.split('_')[1])
+    await callback.answer('Вы выбрали товар')
+    await callback.message.answer(f'Название: {item_data.name}\nОписание: {item_data.description}\nЦена: {item_data.price}₽')
+
+
+@router.callback_query(F.data == 'to_main')
+async def handle_to_main(callback: CallbackQuery):   
+    await callback.answer()
+    await callback.message.delete() 
+    await send_welcome(callback.message)  
